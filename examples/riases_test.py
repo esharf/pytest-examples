5f608d6f34f61fef3abcb745a06b81c4ea5bbe81
esharf <esahrf@paloaltonetworks.com>
2022-02-09 13:31:21 +0200

    reorder examples/riases_test.py

--- examples/riases_test.py
+++ examples/riases_test.py
@@ -12,12 +12,15 @@ def test_raise_with_parent_xception():
         raise ChildException('this test should raise an exception.')
 
 
-def test_raise_with_message_match():
-    with pytest.raises(ParentException, match='this.+specific.+'):
-        raise ChildException('this test should raise a specific exception.')
-
-
 @pytest.mark.xfail      # explained in examples/markers_test.py
 def test_fail_with_unraised_exception():
     with pytest.raises(ParentException, match='this.+specific.+'):
         pass
+
+
+'''You can also use regex to check the exception'''
+
+
+def test_raise_with_message_match():
+    with pytest.raises(ParentException, match='this.+specific.+'):
+        raise ChildException('this test should raise a specific exception.')
